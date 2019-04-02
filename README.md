9c771a83898e440473de3836d84fd4ef9c51c835
Claudiiaa <47463136+Claudiiaa@users.noreply.github.com>
2019-04-02 18:47:30 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# sustainable-food
+# project-codefirst-2019
+this is a commit 
---
+++ js/background.js
@@ -0,0 +1,19 @@
+// writes to the log 'hello'
+console.log('hello!')
+
+// Only fires logic after the page has fully loaded
+$( document ).ready(function(){
+
+$('.carousel').carousel(function(event){
+  interval: 2000
+
+})
+
+$('#carousel').on('slide.bs.carousel', function () {
+
+    $('.carousel').slide.bs.carousel(function(event){
+    $('cycle')
+    $('pause')
+    $('prev')
+    $('next')
+})
